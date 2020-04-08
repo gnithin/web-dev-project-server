c1c1851e29cf9eba08080c0abbda4cc91de4b1ce
Nithin <nithin.linkin@gmail.com>
2020-04-08 18:43:43 -0400

    [auth] Add a blueprint of apis required for registration

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,8 +1,26 @@
-import { Controller, Get } from '@overnightjs/core';
+import { Controller, Get, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
 
 @Controller('api/users')
 export class UserController {
+    @Post('authenticate')
+    private authenticateUser(req: Request, resp: Response) {
+        // TODO:
+        return resp.status(200).json({todo: 'todo'});
+    }
+
+    @Get('details/:user')
+    private getUserDetails(req: Request, resp: Response) {
+        // TODO:
+        return resp.status(200).json({todo: 'todo'});
+    }
+
+    @Get('register')
+    private registerUser(req: Request, resp: Response) {
+        // TODO:
+        return resp.status(200).json({todo: 'todo'});
+    }
+
     @Get('all')
     private getAllUsers(req: Request, resp: Response) {
         return resp.status(200).json({'status': 'success!'})
