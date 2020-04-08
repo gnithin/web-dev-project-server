e06e3fa1452ead25a9a4ea0bad543de7b5b2e113
Nithin <nithin.linkin@gmail.com>
2020-04-08 18:43:24 -0400

    [auth] Add the additional entries to the users table required for authentication

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,12 +1,26 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Question } from './question';
 import { Answer } from './answer';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 @Entity({name: 'users'})
 export class User {
     @PrimaryGeneratedColumn()
     id: number;
 
+    @IsEmail()
+    @Column()
+    email: string;
+
+    @IsNotEmpty()
+    @Column()
+    passwordHash: string;
+
+    @IsNotEmpty()
+    @Column()
+    salt: string;
+
+    @IsNotEmpty()
     @Column()
     name: string;
 
