d7b21e3227c7fb795a7420dcc5d76b23969e8d45
Nithin <nithin.linkin@gmail.com>
2020-04-09 04:32:38 -0400

    [auth] Add correct response types

--- src/common/auth/authMiddleware.ts
+++ src/common/auth/authMiddleware.ts
@@ -1,23 +1,29 @@
 import { Request, Response } from 'express';
 import UserAuth from '../../models/UserAuth';
+import { ResponseHandler } from '../ResponseHandler';
+import ERROR_CODES from '../../constants/errorCodes';
 
 export const UserAuthMiddleware = (req: Request, resp: Response, next: any) => {
     if (!req.isAuthenticated()) {
-        resp.status(401).json({message: 'Unauthorized access!'});
+        ResponseHandler.sendErrorJson(
+            resp,
+            'Unauthorized access!',
+            ERROR_CODES.UNAUTHORIZED_ACCESS,
+            401
+        );
         return;
     }
     next();
 };
 
 export const AdminUserAuthMiddleware = (req: Request, resp: Response, next: any) => {
-    if (!req.isAuthenticated()) {
-        resp.status(401).json({message: 'Unauthorized access!'});
-        return;
-    }
-
-    let user = req.user as UserAuth;
-    if (!user.isAdmin) {
-        resp.status(401).json({message: 'Unauthorized access!'});
+    if (!req.isAuthenticated() || !(req.user as UserAuth).isAdmin) {
+        ResponseHandler.sendErrorJson(
+            resp,
+            'Unauthorized access!',
+            ERROR_CODES.UNAUTHORIZED_ACCESS,
+            401
+        );
         return;
     }
     next();
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,53 +3,50 @@ import { Request, Response } from 'express';
 import { AdminUserAuthMiddleware, UserAuthMiddleware } from '../common/auth/authMiddleware';
 import * as passport from 'passport';
 import authConstants from '../constants/auth';
+import { ResponseHandler } from '../common/ResponseHandler';
+import ERROR_CODES from '../constants/errorCodes';
 
 
 @Controller('api/users')
 export class UserController {
     @Get('login')
     @Middleware(
-        passport.authenticate(
-            authConstants.LOCAL,
-            {
-                session: true,
-            }
-        )
+        [
+            passport.authenticate(authConstants.LOCAL, {session: true}),
+            UserAuthMiddleware,
+        ]
     )
     private loginUser(req: Request, resp: Response) {
-        // TODO: Detect if there is actually a user available already
-        if (req.isAuthenticated()) {
-            return resp.status(200).json({data: req.user});
-        }
-        return resp.status(401).json({message: 'couldn\'t login'});
+        return ResponseHandler.sendSuccessJson(resp, null);
     }
 
     @Get('logout')
     private logoutUser(req: Request, resp: Response) {
         if (!req.isAuthenticated()) {
-            return resp.status(400).json({message: 'couldn\'t logout'});
+            ResponseHandler.sendErrorJson(resp, 'Invalid operation', ERROR_CODES.BAD_REQUEST, 400)
+            return;
         }
 
         req.logOut();
-        return resp.status(200).json({message: 'success!'});
+        return ResponseHandler.sendSuccessJson(resp, null);
     }
 
     @Get('register')
     private registerUser(req: Request, resp: Response) {
         // TODO:
-        return resp.status(200).json({todo: 'todo'});
+        return ResponseHandler.sendSuccessJson(resp, {todo: 'todo'});
     }
 
     @Get('details/:user')
     @Middleware(UserAuthMiddleware)
     private getUserDetails(req: Request, resp: Response) {
         // TODO:
-        return resp.status(200).json({details: 'all-the user details'});
+        return ResponseHandler.sendSuccessJson(resp, {todo: 'user-details'});
     }
 
     @Get('all')
     @Middleware(AdminUserAuthMiddleware)
     private getAllUsers(req: Request, resp: Response) {
-        return resp.status(200).json({'status': 'success!'})
+        return ResponseHandler.sendSuccessJson(resp, {todo: 'all-user-details'});
     }
 }
