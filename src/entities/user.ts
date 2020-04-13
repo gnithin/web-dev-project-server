ba03bd291f255e8f8ba4cd231b6b518b38951550
Tanmay Naik <tnaik96@gmail.com>
2020-04-13 02:02:02 -0400

    add missing question reputation column in user

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,3 +1,4 @@
+import { QuestionReputationPoint } from './questionReputationPoint';
 import { AnswerReputationPoint } from './answerReputationPoint';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn, JoinColumn } from 'typeorm';
 import { Question } from './question';
@@ -32,6 +33,10 @@ export class User {
     @OneToMany(type => AnswerReputationPoint, reputationPoint => reputationPoint.srcUser)
     @JoinColumn({name: 'given_reputations'})
     givenReputations: AnswerReputationPoint[];
+    
+    @OneToMany(type => QuestionReputationPoint, reputationPoint => reputationPoint.srcUser)
+    @JoinColumn({name: 'given_question_reputations'})
+    givenQuestionReputations: QuestionReputationPoint[];
 
     @Column({default: false})
     isAdmin: boolean;
